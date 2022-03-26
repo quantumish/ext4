c946f0af64fb04a3acdbcdc413538e1551fef834
David Freifeld <freifeld.david@gmail.com>
2022-03-25 21:58:05 -0700

    Initial boilerplate for a rewrite

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,13 @@
-// this will let us read integers of various width
-use byteorder::{LittleEndian, ReadBytesExt};
-use positioned_io::{ReadAt, Cursor, Slice};
-use std::fs::OpenOptions;
+// // this will let us read integers of various width
+// use byteorder::{LittleEndian, ReadBytesExt};
+// use positioned_io::{ReadAt, Cursor, Slice};
+use std::fs::File;
+use std::path::Path;
+use std::io::{Read, Seek, SeekFrom};
 use anyhow::{Result, anyhow};
 
-struct Reader<IO: ReadAt> {
-  inner: IO,
-}
-
-#[derive(Debug)]
 struct Superblock {
-    inode_count: u64,
+	inode_count: u64,
     block_count: u64,
     su_blocks: u64,
     free_block_count: u64,
@@ -23,50 +20,55 @@ struct Superblock {
     inodes_per_group: u64,
 }
 
-impl Superblock {
-    fn parse (loc: &dyn ReadAt) -> Result<Superblock> {
-	let r = Reader::new(Slice::new(loc, 1024, None));
-	if r.u16(0x38)? != 0xEF53 {
-	    Err(anyhow!("Magic number is incorrect!"))
-	} else {
-	    Ok(Superblock {
-		inode_count: r.u32(0x0)? as u64,
-		block_count: r.u32(0x4)? as u64,
-		su_blocks: r.u32(0x8)? as u64,
-		free_block_count: r.u32(0xC)? as u64,
-		free_inode_count: r.u32(0x10)? as u64,
-		first_data_block: r.u32(0x14)? as u64,
-		block_size: 2u64.pow(10u32+(r.u32(0x18)?)),
-		cluster_size: r.u32(0x1C)? as u64,
-		blocks_per_group: r.u32(0x20)? as u64,
-		clusters_per_group: r.u32(0x24)? as u64,
-		inodes_per_group: r.u32(0x28)? as u64,		
-	    })
+enum Element {
+	Blank,
+	Superblock(Superblock),	
+}
+
+struct Reader {
+	file: File,
+	offset: usize,
+}
+
+impl Reader {
+	fn new(p: &Path) -> Self {
+		Reader {
+			file: File::open(p).unwrap(),
+			offset: 0,
+		}
 	}
-    }
+	
+	fn next(&mut self) -> Element {
+		if self.offset == 0 {
+			if cfg!(debug_assertions) {
+				let mut buf: [u8; 1024] = [1; 1024];
+				self.file.read(&mut buf);
+				print!("Checking if group 0 padding is all 0... ");
+				for i in buf {
+					assert_eq!(i, 0);
+				}
+				println!(" check!");
+			} else {
+				self.file.seek(SeekFrom::Current(1024));
+			}
+			self.offset += 1024;
+			return Element::Blank;
+		}
+		else if self.offset == 1024 {
+			let mut buf = [0; 1024];
+			self.file.read(&mut buf);
+			print!("Checking if superblock magic number is valid... ");
+			assert_eq!(buf[0x38], 0x53);
+			assert_eq!(buf[0x39], 0xEF);
+			println!(" check!");			
+		}
+		Element::Blank
+	}
 }
 
-impl<IO: ReadAt> Reader<IO> {
-    fn new(inner: IO) -> Self {
-        Self { inner }
-    }
-
-    fn u16(&self, offset: u64) -> Result<u16> {
-        let mut cursor = Cursor::new_pos(&self.inner, offset);
-        Ok(cursor.read_u16::<LittleEndian>()?)
-    }
-
-    fn u32(&self, offset: u64) -> Result<u32> {
-        let mut cursor = Cursor::new_pos(&self.inner, offset);
-        Ok(cursor.read_u32::<LittleEndian>()?)
-    }
-
-}
-
-// using our new result type:
-fn main() -> Result<()> {
-    let file = OpenOptions::new().read(true).open("/dev/sda1")?;
-    let s: Superblock = Superblock::parse(&file)?;
-    println!("{:#?}", s);
-    Ok(())
+fn main() {
+	let p = Path::new("/dev/nvme0n1p3");
+	let mut r = Reader::new(&p);
+	r.next();
+	r.next();
 }
